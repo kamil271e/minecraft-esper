86991b0ec26375330f3fa4b60eb11f5356812565
kamil <kamil271e@gmail.com>
2024-03-12 20:50:37 +0100

    First task

--- src/com/example/bigdata/EsperClient.java
+++ src/com/example/bigdata/EsperClient.java
@@ -24,14 +24,24 @@ public class EsperClient {
 
         if (args.length < 2) {
             noOfRecordsPerSec = 2;
-            howLongInSec = 5;
+            howLongInSec = 10;
         } else {
             noOfRecordsPerSec = Integer.parseInt(args[0]);
             howLongInSec = Integer.parseInt(args[1]);
         }
 
         Configuration config = new Configuration();
-        EPCompiled epCompiled = getEPCompiled(config);
+        String sampleQuery = """
+            @public @buseventtype create json schema MinecraftEvent(ore string, depth int, amount int, ets string, its string);
+            @name('answer') SELECT ore, depth, amount, ets, its
+                  from MinecraftEvent#ext_timed(java.sql.Timestamp.valueOf(its).getTime(), 3 sec)
+            """;
+        String task1Query = """
+            @public @buseventtype create json schema MinecraftEvent(ore string, depth int, amount int, ets string, its string);
+            @name('answer') SELECT ore, amount, sum(amount) as sumAmount
+                  from MinecraftEvent#ext_timed(java.sql.Timestamp.valueOf(its).getTime(), 60 sec) where ore='diamond'
+            """;
+        EPCompiled epCompiled = getEPCompiled(config, task1Query);
 
         // Connect to the EPRuntime server and deploy the statement
         EPRuntime runtime = EPRuntimeProvider.getRuntime("http://localhost:port", config);
@@ -53,8 +63,36 @@ public class EsperClient {
             }
         });
 
-        Faker faker = new Faker();
+        taskRunner(noOfRecordsPerSec, 120, runtime);
 
+    }
+
+    private static EPCompiled getEPCompiled(Configuration config, String query) {
+        CompilerArguments compilerArgs = new CompilerArguments(config);
+
+        // Compile the EPL statement
+        EPCompiler compiler = EPCompilerProvider.getCompiler();
+        EPCompiled epCompiled;
+        try {
+            epCompiled = compiler.compile(query, compilerArgs);
+        }
+        catch (EPCompileException ex) {
+            // handle exception here
+            throw new RuntimeException(ex);
+        }
+        return epCompiled;
+    }
+
+    static void waitToEpoch() throws InterruptedException {
+        long millis = System.currentTimeMillis();
+        Instant instant = Instant.ofEpochMilli(millis) ;
+        Instant instantTruncated = instant.truncatedTo( ChronoUnit.SECONDS ) ;
+        long millis2 = instantTruncated.toEpochMilli() ;
+        TimeUnit.MILLISECONDS.sleep(millis2+1000-millis);
+    }
+
+    public static void taskRunner(int noOfRecordsPerSec, int howLongInSec, EPRuntime runtime) throws InterruptedException {
+        Faker faker = new Faker();
         long startTime = System.currentTimeMillis();
         while (System.currentTimeMillis() < startTime + (1000L * howLongInSec)) {
             waitToEpoch();
@@ -73,33 +111,6 @@ public class EsperClient {
                 runtime.getEventService().sendEventJson(record, "MinecraftEvent");
             }
         }
-    }
-
-    private static EPCompiled getEPCompiled(Configuration config) {
-        CompilerArguments compilerArgs = new CompilerArguments(config);
-
-        // Compile the EPL statement
-        EPCompiler compiler = EPCompilerProvider.getCompiler();
-        EPCompiled epCompiled;
-        try {
-            epCompiled = compiler.compile("""
-                    @public @buseventtype create json schema MinecraftEvent(ore string, depth int, amount int, ets string, its string);
-                    @name('answer') SELECT ore, depth, amount, ets, its
-                          from MinecraftEvent#ext_timed(java.sql.Timestamp.valueOf(its).getTime(), 3 sec)""", compilerArgs);
-        }
-        catch (EPCompileException ex) {
-            // handle exception here
-            throw new RuntimeException(ex);
-        }
-        return epCompiled;
-    }
-
-    static void waitToEpoch() throws InterruptedException {
-        long millis = System.currentTimeMillis();
-        Instant instant = Instant.ofEpochMilli(millis) ;
-        Instant instantTruncated = instant.truncatedTo( ChronoUnit.SECONDS ) ;
-        long millis2 = instantTruncated.toEpochMilli() ;
-        TimeUnit.MILLISECONDS.sleep(millis2+1000-millis);
     }
 }
 
